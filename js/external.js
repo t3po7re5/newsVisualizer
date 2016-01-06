769a769f32bc5853214aa6b4939fe3c484847d11
Jeff M <t3po7re5@vt.edu>
2016-01-05 21:27:09 -0500

    Updates, trying to get ng-repeat to work for circles

--- js/external.js
+++ js/external.js
@@ -1,33 +1,53 @@
-newsItem = {title:"Test News Item", longitude:100, latitude:100, description:"This is a description of the test news item"};
-console.log(newsItem.title);
+var app = angular.module('stories', []);
+app.controller('retrieveStories', ['$scope', '$log', '$http', function($scope, $log, $http) {
+  var vm = this;
+  vm.stories = ['story1 + associating circle', 'story2'];
+  $scope.stories = ['story1 + associating circle', 'story2'];
+  $scope.numberOfStories = 4;
+  $scope.color = "blue";
+  $log.info("got here");
+  //HTTP Get example, this should be in a service
+  $http.get("http://www.w3schools.com/angular/customers.php")
+  .then(function (response)
+  {
+    $log.info(response.data.records);
+  });
+  // $scope.circle = {
+  //   xPos : 130,
+  //   yPos : 130,
+  //   color : "orange"
+  // };
+  $scope.circles = [{
+    xPos : 130,
+    yPos : 130,
+    color : "orange"
+  },
+  {
+      xPos : 230,
+      yPos : 230,
+      color : "purple"
+  }];
 
+}]);
+app.controller('mapStories', ['$scope', function($scope) {
+	// function drawCircle() {
+	// 	var canvas = document.getElementById("myCanvas");
+	// 	var ctx = canvas.getContext("2d");
+	// 	ctx.beginPath();
+	// 	ctx.arc(575,550, 100, 0, 2 * Math.PI, false);
+	// 	ctx.fillStyle = "green";
+	// 	ctx.fill();
+	// }
+}]);
 
 function getScript(url, callback) {
    var script = document.createElement('script');
    script.type = 'text/javascript';
    script.src = url;
-
    script.onreadystatechange = callback;
    script.onload = callback;
-
    document.getElementsByTagName('head')[0].appendChild(script);
 }
-
-var app = angular.module('stories', []);
-app.controller('retrieveStories', ['$scope', function($scope) {
-	$scope.stories = ['story1 + associating circle', 'story2'];
-}]);
-app.controller('mapStories', ['$scope', function($scope) {
-	function drawCircle() {
-		var canvas = document.getElementById("myCanvas");
-		var ctx = canvas.getContext("2d");
-		ctx.beginPath();
-		ctx.arc(575,550, 100, 0, 2 * Math.PI, false);
-		ctx.fillStyle = "green";
-		ctx.fill();
-	}
-}]);
-
 
 function drawCircle2() {
 	var canvas = document.getElementById("myCanvas");
